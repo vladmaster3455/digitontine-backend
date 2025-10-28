0c4a9d0b582aaf98455ec535d361bc72940a6cfd
vladmaster3455 <sergeseenghor@esp.sn>
2025-10-28 14:37:50 +0000

    qerr

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const morgan = require('morgan');
 
-
+app.set('trust proxy', 1);
 // Config & Utils
 const connectDB = require('./config/database');
 const { APP } = require('./config/constants');
